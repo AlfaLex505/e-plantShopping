3a31ee70d5d51f23688e0cec78d69cf3b976ef10
alexis <alexisalfaro505@gmail.com>
2025-07-07 14:49:29 -0400

    Create the logic for removing and updating items

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -16,9 +16,15 @@ export const CartSlice = createSlice({
         }
         },
     removeItem: (state, action) => {
+        state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
+        const {name, quantitiy} = action.payload;
 
+        const itemToUpdate = state.items.find(item => item.name === name);
+        if (itemToUpdate) {
+            itemToUpdate.quantitiy = quantitiy;
+        }
     
     },
   },
